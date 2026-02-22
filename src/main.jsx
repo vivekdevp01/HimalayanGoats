60ea671fccae369728c12a340d50bf2aa22f5bdd
Harshit Rawat <Harshitk1204@gmail.com>
2026-02-22 16:02:00 +0530

    added few section\

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { HelmetProvider } from 'react-helmet-async';
-
+import './style.css' // Put this at the top of main.jsx
 createRoot(document.getElementById('root')).render(
  <HelmetProvider>
     <App />
